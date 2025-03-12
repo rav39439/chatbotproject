0151128db67a314548de091b16db6dfeb2c6b08b
rav39439 <ravish.kumar@ispf.ngo>
2025-03-12 21:35:08 +0530

    update

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -8,11 +8,11 @@ import { useForm } from "react-hook-form";
 import axios from 'axios';
 const Login = (props) => {
   // const dispatch = useDispatch();
+  const [errormessage, seterrormessage] = useState("");
   const [loading, setLoading] = useState(false); // Use useState for loading
 
   const {
     register,
-    watch,
     handleSubmit,
     formState: { errors, isValid },
 
